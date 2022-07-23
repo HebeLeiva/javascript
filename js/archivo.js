a67b7d5e206e08d6a2f60e288e977d94132521b0
Hebe Leiva <hebe_leiva@yahoo.com.ar>
2022-07-23 16:02:23 -0300

    primer commit

---
+++ js/archivo.js
@@ -0,0 +1,11 @@
+debugger
+
+let continuar = true
+const numero = parseInt(prompt("Ingrese un número para saber su potencia:"))
+let resultado = numero
+
+while (continuar) {
+    console.log("La potencia es ", resultado = resultado * numero)
+    continuar = confirm("¿Desea conocer la siguiente potencia?")
+
+}
